ce1d267ed2c6ff9c1e3284954c40cfe55711674b
Alec Tecson <tecsonalr@gmail.com>
2023-03-26 22:42:26 +0800

    Section 3 - 25. Overview of Property Panes

---
+++ Section 3/PropertyPaneControls/src/webparts/propertyPaneControls/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IPropertyPaneControlsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'PropertyPaneControlsWebPartStrings' {
+  const strings: IPropertyPaneControlsWebPartStrings;
+  export = strings;
+}
