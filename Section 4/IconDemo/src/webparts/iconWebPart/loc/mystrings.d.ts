389d1f2ba318ba9db67922d6f012a0cc69a89f5b
Alec Tecson <tecsonalr@gmail.com>
2023-04-01 22:26:07 +0800

    39. Modifying Default Icon

---
+++ Section 4/IconDemo/src/webparts/iconWebPart/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IIconWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'IconWebPartWebPartStrings' {
+  const strings: IIconWebPartWebPartStrings;
+  export = strings;
+}
