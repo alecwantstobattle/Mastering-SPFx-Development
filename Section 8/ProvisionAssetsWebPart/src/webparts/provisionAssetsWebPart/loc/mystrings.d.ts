f0d8fb874a1138d3af4526bb0e95abd35b2e79a1
Alec Tecson <tecsonalr@gmail.com>
2023-04-10 23:44:12 +0800

    Section 8 - 52. Creating Site Columns,Site Content Types,Lists through Code

---
+++ Section 8/ProvisionAssetsWebPart/src/webparts/provisionAssetsWebPart/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IProvisionAssetsWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'ProvisionAssetsWebPartWebPartStrings' {
+  const strings: IProvisionAssetsWebPartWebPartStrings;
+  export = strings;
+}
