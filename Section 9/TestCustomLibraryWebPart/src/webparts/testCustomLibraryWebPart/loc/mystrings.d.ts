e88739b33494b07fb1e493a1102456b050e013cb
Alec Tecson <tecsonalr@gmail.com>
2023-04-16 20:21:49 +0800

    55. Test Library locally

---
+++ Section 9/TestCustomLibraryWebPart/src/webparts/testCustomLibraryWebPart/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ITestCustomLibraryWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'TestCustomLibraryWebPartWebPartStrings' {
+  const strings: ITestCustomLibraryWebPartWebPartStrings;
+  export = strings;
+}
