dadcf421a9e3f5644e185e2f21a34cbd12a1eefc
Alec Tecson <tecsonalr@gmail.com>
2023-04-23 18:51:21 +0800

    Section 15 - 81. Introduction To Isolated Web Parts

---
+++ Section 15/IsolatedWebPart/src/webparts/isolatedWebPart/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IIsolatedWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'IsolatedWebPartWebPartStrings' {
+  const strings: IIsolatedWebPartWebPartStrings;
+  export = strings;
+}
