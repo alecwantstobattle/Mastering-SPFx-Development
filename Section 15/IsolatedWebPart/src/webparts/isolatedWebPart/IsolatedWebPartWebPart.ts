c6d191853cf70c45e8ddd92907898922f1caac90
Alec Tecson <tecsonalr@gmail.com>
2023-04-23 19:33:14 +0800

    82. Developing and Testing an Isolated Web Part

--- Section 15/IsolatedWebPart/src/webparts/isolatedWebPart/IsolatedWebPartWebPart.ts
+++ Section 15/IsolatedWebPart/src/webparts/isolatedWebPart/IsolatedWebPartWebPart.ts
@@ -1,7 +1,7 @@
 import { Version } from '@microsoft/sp-core-library';
 import {
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
+  PropertyPaneTextField,
 } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import { IReadonlyTheme } from '@microsoft/sp-component-base';
@@ -10,12 +10,14 @@ import { escape } from '@microsoft/sp-lodash-subset';
 import styles from './IsolatedWebPartWebPart.module.scss';
 import * as strings from 'IsolatedWebPartWebPartStrings';
 
+import { MSGraphClient } from '@microsoft/sp-http';
+import * as MicrosoftGraph from '@microsoft/microsoft-graph-types';
+
 export interface IIsolatedWebPartWebPartProps {
   description: string;
 }
 
 export default class IsolatedWebPartWebPart extends BaseClientSideWebPart<IIsolatedWebPartWebPartProps> {
-
   private _isDarkTheme: boolean = false;
   private _environmentMessage: string = '';
 
@@ -26,39 +28,35 @@ export default class IsolatedWebPartWebPart extends BaseClientSideWebPart<IIsola
   }
 
   public render(): void {
-    this.domElement.innerHTML = `
-    <section class="${styles.isolatedWebPart} ${!!this.context.sdks.microsoftTeams ? styles.teams : ''}">
-      <div class="${styles.welcome}">
-        <img alt="" src="${this._isDarkTheme ? require('./assets/welcome-dark.png') : require('./assets/welcome-light.png')}" class="${styles.welcomeImage}" />
-        <h2>Well done, ${escape(this.context.pageContext.user.displayName)}!</h2>
-        <div>${this._environmentMessage}</div>
-        <div>Web part property value: <strong>${escape(this.properties.description)}</strong></div>
-      </div>
-      <div>
-        <h3>Welcome to SharePoint Framework!</h3>
-        <p>
-        The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It's the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-        </p>
-        <h4>Learn more about SPFx development:</h4>
-          <ul class="${styles.links}">
-            <li><a href="https://aka.ms/spfx" target="_blank">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank">Microsoft 365 Developer Community</a></li>
-          </ul>
-      </div>
-    </section>`;
+    this.context.msGraphClientFactory
+      .getClient()
+      .then((graphclient: MSGraphClient): void => {
+        graphclient
+          .api('/me')
+          .get((error, user: MicrosoftGraph.User, rawResponse?: any) => {
+            this.domElement.innerHTML = `
+          <div>
+          <p>Display Name: ${user.displayName}</p>
+          <p>Given Name: ${user.givenName}</p>
+          <p>Surname: ${user.surname}</p>
+          <p>Email ID: ${user.mail}</p>
+          <p>Mobile Phone: ${user.mobilePhone}</p>   
+          </div>`;
+          });
+      });
   }
 
   private _getEnvironmentMessage(): string {
-    if (!!this.context.sdks.microsoftTeams) { // running in Teams
-      return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+    if (!!this.context.sdks.microsoftTeams) {
+      // running in Teams
+      return this.context.isServedFromLocalhost
+        ? strings.AppLocalEnvironmentTeams
+        : strings.AppTeamsTabEnvironment;
     }
 
-    return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment;
+    return this.context.isServedFromLocalhost
+      ? strings.AppLocalEnvironmentSharePoint
+      : strings.AppSharePointEnvironment;
   }
 
   protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
@@ -67,13 +65,13 @@ export default class IsolatedWebPartWebPart extends BaseClientSideWebPart<IIsola
     }
 
     this._isDarkTheme = !!currentTheme.isInverted;
-    const {
-      semanticColors
-    } = currentTheme;
+    const { semanticColors } = currentTheme;
     this.domElement.style.setProperty('--bodyText', semanticColors.bodyText);
     this.domElement.style.setProperty('--link', semanticColors.link);
-    this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered);
-
+    this.domElement.style.setProperty(
+      '--linkHovered',
+      semanticColors.linkHovered
+    );
   }
 
   protected get dataVersion(): Version {
@@ -85,20 +83,20 @@ export default class IsolatedWebPartWebPart extends BaseClientSideWebPart<IIsola
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: strings.PropertyPaneDescription,
           },
           groups: [
             {
               groupName: strings.BasicGroupName,
               groupFields: [
                 PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
-              ]
-            }
-          ]
-        }
-      ]
+                  label: strings.DescriptionFieldLabel,
+                }),
+              ],
+            },
+          ],
+        },
+      ],
     };
   }
 }
