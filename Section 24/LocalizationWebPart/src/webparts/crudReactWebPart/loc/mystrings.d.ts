425b86f5f1775806042d21cf6c73495486465959
Alec Tecson <tecsonalr@gmail.com>
2023-05-01 17:42:49 +0800

    Section 24 - 110. Applying Localization Support To SPFx Solution

---
+++ Section 24/LocalizationWebPart/src/webparts/crudReactWebPart/loc/mystrings.d.ts
@@ -0,0 +1,20 @@
+declare interface ICrudReactWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  lblID: string;
+  lblSoftwareTitle: string;
+  lblSoftwareName: string;
+  lblSoftwareDescription: string;
+  lblSoftwareVendor: string;
+  lblSoftwareVersion: string;
+}
+
+declare module 'CrudReactWebPartWebPartStrings' {
+  const strings: ICrudReactWebPartWebPartStrings;
+  export = strings;
+}
