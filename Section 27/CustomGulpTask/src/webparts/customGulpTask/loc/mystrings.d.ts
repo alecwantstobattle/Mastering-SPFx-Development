1d084d9b6bf5e298461ccbabad55c6d1cf2ec86e
Alec Tecson <tecsonalr@gmail.com>
2023-05-02 01:03:12 +0800

    Section 27 - 118. Creating a Custom Gulp Task using Gulp Sequence

---
+++ Section 27/CustomGulpTask/src/webparts/customGulpTask/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ICustomGulpTaskWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'CustomGulpTaskWebPartStrings' {
+  const strings: ICustomGulpTaskWebPartStrings;
+  export = strings;
+}
